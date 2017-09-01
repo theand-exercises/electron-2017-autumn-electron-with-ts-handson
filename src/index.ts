cab6a43590e0f6581e5a775b5a329508f4508fa0
Heesang Chae <theand@gmail.com>
2017-09-01 21:03:02 +0900

    firebase init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import {app, BrowserWindow} from 'electron';
+import * as firebase from 'firebase';
 
-app.on('ready', () => {
-    console.log('ready');
+
+// Initialize Firebase
+const config = {
+    apiKey: 'AIzaSyA9B5I-kb1feALwDhpn4-W18BxbG2CEGfU',
+    authDomain: 'elects-seminar.firebaseapp.com',
+    databaseURL: 'https://elects-seminar.firebaseio.com',
+    projectId: 'elects-seminar',
+    storageBucket: 'elects-seminar.appspot.com',
+    messagingSenderId: '880959694204'
+};
+const firebaseApp = firebase.initializeApp(config);
+
+const auth = firebase.auth();
+auth.onAuthStateChanged((user: { email: string; }) => {
+    console.log(user.email);
 });
 
+app.on('ready', () => {
+    console.log('app ready');
+
+    auth.signInWithEmailAndPassword('email', 'password');
+});
